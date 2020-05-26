1f21eae6ff067381c893472cfd511eca3dae85c5
Jonathan_Sperafico <jhonny.potter@gmail.com>
2020-05-26 21:47:04 +0000

    interface of profile fixed

--- call_cthulhu_app/lib/widgets/profile/profile.dart
+++ call_cthulhu_app/lib/widgets/profile/profile.dart
@@ -7,53 +7,124 @@ class ProfileWidget extends StatefulWidget {
 }
 
 class _ProfileWidgetState extends State<ProfileWidget> {
-
   var _appBar = AppBar(
     title: Text(
       'Your Profile',
     ),
   );
 
+  Widget _disconnectCard(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Text(
+            'You are connected through an \'Google account\'',
+            style: Theme.of(context).textTheme.bodyText1,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        RaisedButton(
+          child: Text(
+            'Disconnect',
+            style: Theme.of(context).textTheme.button,
+          ),
+          onPressed: null,
+        ),
+      ],
+    );
+  }
+
+  Widget _downloadCard(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Text(
+            'Download your user stored data',
+            style: Theme.of(context).textTheme.bodyText1,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        RaisedButton(
+          child: Text(
+            'Download',
+            style: Theme.of(context).textTheme.button,
+          ),
+          onPressed: null,
+        ),
+      ],
+    );
+  }
+
+  Widget _deleteCard(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Delete your data from our database',
+                style: Theme.of(context).textTheme.bodyText1,
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                'This is not reversible!',
+                style: Theme.of(context).textTheme.bodyText2,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+        RaisedButton(
+          child: Text(
+            'Delete me',
+            style: Theme.of(context).textTheme.button,
+          ),
+          onPressed: null,
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar,
       drawer: WelcomeApp.drawer,
-      body: Column(
-        children: <Widget>[
-          Card(
-            child: Column(
-              children: <Widget>[
-                Text('Profile Name'),
-                Text('Your connected to this app through \'Google account\''),
-                RaisedButton(
-                  child: Text('Disconnect'),
-                  onPressed: null,
-                ),
-              ],
+      body: Flex(
+        direction: Axis.vertical,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Flexible(
+            child: Card(
+              margin: EdgeInsets.symmetric(vertical: 5),
+              elevation: 5,
+              child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(10),
+                  child: this._disconnectCard(context)),
             ),
           ),
-          Card(
-            child: Column(
-              children: <Widget>[
-                Text('You can download your app data through button bellow.'),
-                RaisedButton(
-                  child: Text('Download here'),
-                  onPressed: null,
-                ),
-              ],
+          Flexible(
+            child: Card(
+              margin: EdgeInsets.symmetric(vertical: 5),
+              elevation: 5,
+              child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(10),
+                  child: this._downloadCard(context)),
             ),
           ),
-          Card(
-            child: Column(
-              children: <Widget>[
-                Text('You can delete your account any time you want.'),
-                Text('We warn you that this action isn\'t reversible.'),
-                RaisedButton(
-                  child: Text('Delete me from your database'),
-                  onPressed: null,
-                ),
-              ],
+          Flexible(
+            child: Card(
+              margin: EdgeInsets.symmetric(vertical: 5),
+              elevation: 5,
+              child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(10),
+                  child: this._deleteCard(context)),
             ),
           ),
         ],
